8ee218b4d84a3a6957137b6bc57a48bd797f4265
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-03-02 20:34:14 +0900

    Create 58. Length of Last Word.java

---
+++ 58. Length of Last Word.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] split = s.split(" ");
+        
+        return split.length == 0 ? 0 : split[split.length - 1].length();
+    }
+}
