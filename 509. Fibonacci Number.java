64e1cf0acc06b7302b6909a79dafda446dfaba19
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-03-09 21:04:07 +0900

    Update 509. Fibonacci Number.java

--- 509. Fibonacci Number.java
+++ 509. Fibonacci Number.java
@@ -1,7 +1,12 @@
-class Solution {
+lass Solution {
+    Map<Integer,Integer> mem = new HashMap();
     public int fib(int N) {
         if(N == 0) return 0;
         if(N == 1) return 1;
-        return fib(N-1) + fib(N-2);
+        if(mem.get(N) != null) {
+            return mem.get(N);
+        }
+        mem.put(N, fib(N-1) + fib(N-2));
+        return mem.get(N);
     }
 }
