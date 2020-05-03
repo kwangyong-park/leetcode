6274b49c7460066376dc6fe273ec71e70445c857
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-05-03 22:59:45 +0900

    Update 383. Ransom Note.java

--- 383. Ransom Note.java
+++ 383. Ransom Note.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
         for(Character c : ransomNote.toCharArray()) {
-            int cnt = map.getOrDefault(c, 0);
-            map.put(c, cnt+1);
+            int cnt = map1.getOrDefault(c, 0);
+            map1.put(c, cnt+1);
         }
-        
+
         for(Character c : magazine.toCharArray()) {
-            int cnt = map.getOrDefault(c, -1);
-            if(cnt > 1) {
-                map.put(c, cnt-1);
-            } else {
-                map.remove(c);
-            }
+            int cnt = map2.getOrDefault(c, 0);
+            map2.put(c, cnt+1);
         }
-        return map.size() > 0 ? false: true;
-        
+
+        for(Map.Entry<Character, Integer> e : map1.entrySet()) {
+            Integer val = map2.get(e.getKey());
+            if(val == null) return false;
+            else if(val < e.getValue()) return false;
+            
+        }
+        return true;
     }
 }
