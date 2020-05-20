ad20cd8f063d38f0bbd88946b345bb8c798eb9d4
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-05-21 08:12:06 +0900

    Update 230. Kth Smallest Element in a BST.java

--- 230. Kth Smallest Element in a BST.java
+++ 230. Kth Smallest Element in a BST.java
@@ -2,16 +2,16 @@ class Solution {
     List<Integer> list = new ArrayList<>();
     public int kthSmallest(TreeNode root, int k) {
         rec(root, k);
-        Collections.sort(list);
         return list.get(k-1);
     }
     
     public void rec(TreeNode root, int k) {
-        if(root == null) return ;
-        
-        list.add(root.val);
-        
+        if(root == null) return;
         rec(root.left, k);
+        list.add(root.val);
+        if(root.val == k) {
+            return;
+        }
         rec(root.right, k);
     }
 }
