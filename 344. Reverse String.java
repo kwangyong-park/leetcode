b04a22f47d651bbb141119bae4eeb07919de39a5
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-06-04 19:43:31 +0900

    Update 344. Reverse String.java

--- 344. Reverse String.java
+++ 344. Reverse String.java
@@ -1,14 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length - 1;
-        while(left < right) {
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
+        int n = s.length/2;
+        for(int i = 0; i < n; i++) {
+            char temp = s[i];
+            int j = s.length - i - 1;
+            s[i] = s[j];
+            s[j] = temp;
         }
-        
     }
 }
