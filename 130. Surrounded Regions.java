8da6f56de2abf8ed2ed9250a67a831a0d81ac182
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-06-17 00:23:16 +0900

    Update 130. Surrounded Regions.java

--- 130. Surrounded Regions.java
+++ 130. Surrounded Regions.java
@@ -2,18 +2,21 @@ class Solution {
     Stack<Pair<Integer,Integer>> stack = new Stack<>();
     Set<String> check = new HashSet<>();
     public void solve(char[][] board) {
-        for(int i = 0; i < board.length; i++)  {
-            if(board[0][i] == 'O')
-                push(new Pair(0, i));
-            if(board[board.length - 1][i] == 'O')
-                push(new Pair(board.length - 1, i));
-        }
-
-        for(int i = 0; i < board[0].length; i++) {
+        if(board.length == 0) return;
+        int row = board.length;
+        int col = board[0].length; 
+        for(int i = 0; i < row; i++)  {
             if(board[i][0] == 'O')
                 push(new Pair(i, 0));
-            if(board[i][board.length-1] == 'O')
-                push(new Pair(i, board.length-1));
+            if(board[i][col-1] == 'O')
+                push(new Pair(i, col-1));
+        }
+
+        for(int i = 0; i < col; i++) {
+            if(board[0][i] == 'O')
+                push(new Pair(0, i));
+            if(board[row-1][i] == 'O')
+                push(new Pair(row-1, i));
         }
 
         int[][] arrow = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
