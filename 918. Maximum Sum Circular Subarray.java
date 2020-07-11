a80771b881925858a100cda01dce943e0509ccaf
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-07-11 09:21:11 +0900

    Update 918. Maximum Sum Circular Subarray.java

--- 918. Maximum Sum Circular Subarray.java
+++ 918. Maximum Sum Circular Subarray.java
@@ -1,25 +1,20 @@
 class Solution {
-   public int maxSubarraySumCircular(int[] A) {
-        int ret = Integer.MIN_VALUE;
-        for(int i = 0 ; i < A.length; i++) {
-            int val = A[i];
-            int max = A[i];
-            for(int j = i + 1; j < A.length; j++) {
-                ret = Math.max(ret, A[j] + val);
-                val = Math.max(val + A[j], 0);
-                max = Math.max(A[j], max);
-            }
-            for(int j = 0; j < i; j++) {
-                ret = Math.max(ret, A[j] + val);
-                val = val + A[j] >= 0 ? val + A[j] : 0;
-                max = Math.max(A[j], max);
-            }
-            if(max < 0) {
-                ret = Math.max(max, ret);
-            } else {
-                ret = Math.max(val, ret);
-            }
+    public int maxSubarraySumCircular(int[] A) {
+        int maxCurrent = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int minCurrent = 0;
+        int minSum = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int i : A) {
+            maxCurrent = Math.max(i, maxCurrent + i);
+            maxSum = Math.max(maxSum, maxCurrent);
+            minCurrent = Math.min(i, minCurrent + i);
+            minSum = Math.min(minCurrent, minSum);
+            sum+=i;
         }
-        return ret;
+        if(maxSum > 0) 
+            return Math.max(maxSum, sum - minSum);
+        else 
+            return maxSum;
     }
 }
