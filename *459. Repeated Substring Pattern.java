62291d794158c7b8f379b5204ab9f9625ffa3afd
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-09-03 22:33:48 +0900

    Create *459. Repeated Substring Pattern.java

---
+++ *459. Repeated Substring Pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+  public boolean repeatedSubstringPattern(String s) {
+        if(s.length() == 0) return false;
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        int left = 0;
+        int right = s.length() -1;
+        while(left <= right) {
+            if(left == right){
+                queue.add(s.charAt(left++));
+                break;
+            }
+            queue.add(s.charAt(left++));
+            stack.add(s.charAt(right--));
+        }
+        if(stack.size() != queue.size()) return false;
+
+        while(!queue.isEmpty()) {
+            if(queue.poll() != stack.pop()) return false;
+        }
+        return true;
+    }
+}
