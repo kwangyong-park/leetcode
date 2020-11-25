5a5e0dae9afb82088ad24abad87e3ad03bd440fb
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2020-11-25 22:18:46 +0900

    Create 606. Construct String from Binary Tree.java

---
+++ 606. Construct String from Binary Tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String tree2str(TreeNode t) {
+        if(t == null) {
+            return  "";
+        }
+        if(t.left == null && t.right == null) {
+            return t.val + "";
+        }
+        String left =  t.left != null ? "("+ tree2str(t.left) +")" : "()";
+        String right = t.right != null ?"(" + tree2str(t.right) + ")": "";
+        
+        return t.val + left + right;        
+    }
+}
