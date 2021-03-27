323bee224a1d959fa1ccc98d7d0d47148895b0b4
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2021-03-27 12:08:10 +0900

    Update 617. Merge Two Binary Trees.java

--- 617. Merge Two Binary Trees.java
+++ 617. Merge Two Binary Trees.java
@@ -1,36 +1,14 @@
 class Solution {
-    
- public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if(t1 == null && t2 == null) return null;
-        if(t1 == null) t1 = new TreeNode(0);
-        if(t2 == null) t2 = new TreeNode(0);
-        TreeNode t = new TreeNode(t1.val + t2.val);
-        merge(t1, t2, t);
-        return t;
-    }
-
-    public void merge(TreeNode t1, TreeNode t2, TreeNode parent) {
-        if(t1 == null && t2 == null) return;
-
-        if((t1 != null && t1.left != null) || (t2 != null &&  t2.left != null)) {
-            int sum = 0;
-            if(t1 != null && t1.left != null) sum+=t1.left.val;
-            if(t2 != null && t2.left != null) sum+=t2.left.val;
-            parent.left = new TreeNode(sum);
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null) {
+            return null;
         }
-        if((t1 != null && t1.right != null) || (t2 != null &&  t2.right != null)) {
-            int sum = 0;
-            if(t1 != null && t1.right != null) sum+=t1.right.val;
-            if(t2 != null && t2.right != null) sum+=t2.right.val;
-            parent.right = new TreeNode(sum);
-        }
-
-        if(parent.left != null)
-            merge(t1.left, t2.left, parent.left);
-        if(parent.right != null)
-            merge(t1.right, t2.right, parent.right);
-
+        int rootVal = 0;
+        if(root1 != null) rootVal+=root1.val;
+        if(root2 != null) rootVal+=root2.val;
+        TreeNode result = new TreeNode(rootVal);
+        result.left = mergeTrees(root1 !=null ? root1.left: null, root2 !=null ? root2.left: null);
+        result.right = mergeTrees(root1 !=null ? root1.right: null, root2 !=null ? root2.right: null);
+        return result;
     }
-    
-    
 }
