1c7478c30f337cfee1964ff580ff42fea88fadef
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2022-02-25 20:35:53 +0900

    Update 165. Compare Version Numbers.java

--- 165. Compare Version Numbers.java
+++ 165. Compare Version Numbers.java
@@ -1,22 +1,15 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        List<String> versions1List = new ArrayList<>();
-        List<String> versions2List = new ArrayList<>();
-        Collections.addAll(versions1List, v1);
-        Collections.addAll(versions2List, v2);
-        while(versions1List.size() != versions2List.size()) {
-            if(versions1List.size() > versions2List.size()) {
-               versions2List.add("0");
-            } else {
-                versions1List.add("0");
-            }
+        String[] str1 = version1.split("\\.");
+        String[] str2 = version2.split("\\.");
+        int max = Math.max(str1.length,str2.length);
+        for(int i=0;i<max;i++){
+            int num1 = i >= str1.length ? 0 : Integer.parseInt(str1[i]);
+            int num2 = i >= str2.length ? 0 : Integer.parseInt(str2[i]);
+            if(num1 < num2) return -1;
+            if(num1 > num2) return 1;
         }
-        for(int i = 0; i < versions1List.size(); i++) {
-            if(Integer.parseInt(versions1List.get(i)) > Integer.parseInt(versions2List.get(i))) return 1;
-            else if(Integer.parseInt(versions1List.get(i)) < Integer.parseInt(versions2List.get(i))) return -1;
-        }
+
         return 0;
     }
 }
