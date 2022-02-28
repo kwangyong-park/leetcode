80221145bc58212b8f402129f44570fe3d264c1b
dongne.dev <2535495+kwnagyong@users.noreply.github.com>
2022-02-28 19:14:04 +0900

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,11 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) 
-            for(int j = i+1; j < nums.length; j++) 
-                if(nums[i] + nums[j] == target) 
-                    return new int[]{i, j};
-            
+        Map<Integer, Integer> numToIndex = new HashMap<>();
         
-        return new int[0];
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                int[] result = { numToIndex.get(complement), i };
+                return result;
+            }
+            numToIndex.put(nums[i], i);
+        }
+        
+        throw new IllegalStateException();
     }
 }
