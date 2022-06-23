c5a6f014105a94f2e906ee9e72d529a711490e77
dongne.dev <2535495+kwangyong-park@users.noreply.github.com>
2022-06-24 07:49:48 +0900

    Update 215. Kth Largest Element in an Array.java

--- 215. Kth Largest Element in an Array.java
+++ 215. Kth Largest Element in an Array.java
@@ -1,40 +1,20 @@
 class Solution {
-  
-    public int[] merge(int[] left, int[] right) {
-        int lp = 0;
-        int rp = 0;
-        int pivot = 0;
-        int leftSize = left.length;
-        int rightSize = right.length ;
-        int[] result = new int[left.length + right.length];
-        while(leftSize > lp || rightSize > rp) {
-            if(leftSize > lp && rightSize > rp) {
-                if(left[lp] > right[rp]) {
-                    result[pivot++] = left[lp++];
-                } else {
-                    result[pivot++] = right[rp++];
-                }
-            } else if(leftSize > lp) {
-                result[pivot++] = left[lp++];
-            } else if(rightSize > rp) {
-                result[pivot++] = right[rp++];
-            }
-        }
-        return result;
-    }
+	public int findKthLargest(int[] nums, int k) {
 
-    public int[] div(int[] nums) {
-        if(nums.length <= 1) {
-            return nums;
-        }
-        int mid = nums.length / 2;
-        int[] left = div(Arrays.copyOfRange(nums, 0, mid));
-        int[] right = div(Arrays.copyOfRange(nums, mid, nums.length));
-        return merge(left, right);
-    }
+		PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
+		for(int i = 0; i < nums.length; i++){
+			q.add(nums[i]);
+		}
 
-    public int findKthLargest(int[] nums, int k) {
-        int[] result = div(nums);
-        return result[k-1];
-    }
+		Integer ans = -1;
+		for(int i = 0;i <k; i++) {
+			if(i == k-1) {
+				ans = q.poll();
+				return ans;
+			}
+			q.poll();
+		}
+
+		return -1;
+	}
 }
