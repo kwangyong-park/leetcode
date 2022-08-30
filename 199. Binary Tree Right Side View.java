cd9e59c33a1d616fe503f1d396435123676e6c59
dongne.dev <2535495+kwangyong-park@users.noreply.github.com>
2022-08-31 06:14:56 +0900

    Update 199. Binary Tree Right Side View.java

--- 199. Binary Tree Right Side View.java
+++ 199. Binary Tree Right Side View.java
@@ -1,27 +1,20 @@
 class Solution {
-    Map<Integer, Pair<Integer, Integer>> map = new HashMap<>();
-    int max = 0;
+    Map<Integer, TreeNode> map = new HashMap<>();
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ret = new ArrayList<>();
-        if(root == null) return ret;
-        perm(root, 0, 1);
-        for(int i = 0 ; i <= max; i++) {
-            ret.add(map.get(i).getValue());
+        List<Integer> ans = new ArrayList<>();
+        rec(root, 0, 0);
+        for(int i = 0; i < map.size(); i++) {
+            ans.add(map.get(i).val);
         }
-        return ret;
+        return ans;
     }
-    public void perm(TreeNode root, int step, int po) {
-        if(root == null) return;
-        if(map.get(step) == null) {
-            map.put(step, new Pair(po, root.val));
-        } else {
-            Pair<Integer, Integer> old = map.get(step);
-            if(old.getKey() < po) {
-                map.put(step, new Pair(po, root.val));
-            }
+
+    public void rec(TreeNode root, int depth, int weight) {
+        if(root == null)  {
+            return;
         }
-        perm(root.right, step + 1, po);
-        perm(root.left, step + 1, -1);
-        max = Math.max(max, step);
+        map.put(depth, root);
+        rec(root.left, depth + 1, weight);
+        rec(root.right, depth + 1, weight + 1);
     }
 }
