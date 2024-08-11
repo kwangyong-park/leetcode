d76e42d786d52e5900ab14648c3fa6f121cb288c
dongne.dev <2535495+kwangyong-park@users.noreply.github.com>
2024-08-11 21:25:13 +0900

    Update *1442. Count Triplets That Can Form Two Arrays of Equal XOR.kt

--- *1442. Count Triplets That Can Form Two Arrays of Equal XOR.kt
+++ *1442. Count Triplets That Can Form Two Arrays of Equal XOR.kt
@@ -2,7 +2,6 @@ class Solution {
     fun countTriplets(arr: IntArray): Int {
         val n = arr.size
         val prefix = IntArray(n + 1)
-
         for (i in 0 until n) {
             prefix[i + 1] = prefix[i] xor arr[i]
         }
